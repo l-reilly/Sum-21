6dbe0b913974436ec3492cfddd3c75daad3761d7
Lilli Reilly <lilli.reilly@gmail.com>
2021-10-23 11:29:32 +0200

    Issues displaying cards

--- Javafiles/deck.js
+++ Javafiles/deck.js
@@ -1,28 +1,26 @@
 
 //Remove cardValues
 const cardValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const cardImages = ["images/card 1.jpg", 2, 3, 4, 5, 6, 7, "images/card 8.jpg", 9, 10]
+const cardImages = ["images/card 1.jpg", "images/card 2.jpg", "images/card 3.jpg", "images/card 4.jpg", "images/card 5.jpg", "images/card 6.jpg", "images/card 7.jpg", "images/card 8.jpg", "images/card 9.jpg", "images/card 10.jpg"]
 let total;
 let randomCard1;
 let randomCard2;
 let startingCards = [];
+let randomCard3;
 
 
 function getStartingCards() {
   // Use Math.Random to get a value between 1-10
   randomCard1 = cardValues[Math.floor(Math.random() * cardValues.length)];
-  document.getElementById("cards-one").setAttribute('img', cardImages[(randomCard1-1)]);
-  //Put tag on visible ( same as pizza builder)
+  document.getElementById("cards-one").setAttribute('src', cardImages[(randomCard1-1)]);
   randomCard2 = cardValues[Math.floor(Math.random() * cardValues.length)];
-  document.getElementById("cards-two").setAttribute('img', cardImages[(randomCard2-1)]);
+  document.getElementById("cards-two").setAttribute('src', cardImages[(randomCard2-1)]);
 }
 
 
-function getCardTotal() {
-  total = randomCard1 + randomCard2;
-  return total;
 
-}
+
+
 
 
 
--- Javafiles/game.js
+++ Javafiles/game.js
@@ -1,19 +1,61 @@
 //get elements
 const startGameDiv = document.querySelector(".start-game");
 const startGameButton = document.querySelector(".btn-start");
-const addCardButton = document.querySelector(".add-card")
-const totalDiv = document.querySelector(".total")
-const cardTotal = document.querySelector(".cardsTotal")
-const nextRoundBtn = document.querySelector(".nxt-rnd")
+const addCardButton = document.querySelector(".add-card");
+const totalDiv = document.querySelector(".total");
+const cardTotal = document.querySelector(".cardsTotal");
+const nextRoundBtn = document.querySelector(".nxt-rnd");
+let displayTotal = document.getElementById("game-total");
+let displayPoints = document.getElementById("point-total")
+let points = 100;
+const losingDiv = document.querySelector("#losing");
+const newGameButton = document.querySelector(".new-game")
+
 
 function startGame() {
     startGameDiv.setAttribute('id', "start-game");
     addCardButton.removeAttribute('id');
     totalDiv.removeAttribute('id');
     getStartingCards();
-   document.getElementById("game-total").textContent = getCardTotal();
+    document.querySelector('.points-space').removeAttribute('class');
+    displayPoints.textContent = checkPoints();
+   displayTotal.textContent = getCardTotal();
 } 
+function getCardTotal() {
+    total = randomCard1 + randomCard2;
+    return total;
+  }
+function addCardThree() {
+    randomCard3 = cardValues[Math.floor(Math.random() * cardValues.length)]
+    total = randomCard1 + randomCard2 + randomCard3;
+    displayTotal.textContent = total
+    checkPoints();
+  }
+
+  function checkPoints() {
+    if (total === 19) {
+        points += 10
+    } 
+    else if (total ===20) {
+        points += 20
+    }
+    else if (total === 21) {
+        points += 100
+    }
+    else if (total > 21) {
+        points -= 100
+    }
+    return points
+    }
+
+    function updateGame() {
+        if (points === 0) {
+            losingDiv.removeAttribute('id')
+            addCardButton.setAttribute('id', 'game-starts');
+          }
+    }
 
 
 startGameButton.addEventListener('click', startGame);
-
+addCardButton.addEventListener('click', addCardThree);
+newGameButton.addEventListener('click', startNewGame)
