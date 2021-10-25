cb372f3b60077e1d86caad953d7f3aab2e165102
Lilli Reilly <lilli.reilly@gmail.com>
2021-10-25 20:36:04 +0200

    Points displaying correctly

--- Javafiles/game.js
+++ Javafiles/game.js
@@ -8,7 +8,7 @@ const nextRoundBtn = document.querySelector(".nxt-rnd");
 const btnFourthCard = document.querySelector(".addFourthCard")
 const btnFifthCard = document.querySelector(".addFifthCard")
 let displayTotal = document.getElementById("game-total");
-let displayPoints = document.getElementById("point-total")
+let displayPoints = document.querySelector("#point-total")
 let points = 100;
 const losingDiv = document.querySelector("#losing");
 const newGameButton = document.querySelector(".new-game")
@@ -21,19 +21,16 @@ function startGame() {
     getStartingCards();
     document.querySelector('.points-space').removeAttribute('class');
     displayPoints.textContent = checkPoints();
-   displayTotal.textContent = getCardTotal();
+    total = randomCard1 + randomCard2;
+   displayTotal.textContent = total;
 
 } 
-function getCardTotal() {
-    total = randomCard1 + randomCard2;
-    return total;
-  }
 function addCardThree() {
     randomCard3 = cardValues[Math.floor(Math.random() * cardValues.length)]
     document.getElementById("cards-three").setAttribute('src', cardImages[(randomCard3-1)])
     total = randomCard1 + randomCard2 + randomCard3;
     displayTotal.textContent = total
-    checkPoints();
+    displayPoints.textContent = checkPoints();
     addCardButton.setAttribute('id', 'start-game')
     btnFourthCard.removeAttribute('id')
   }
@@ -42,7 +39,7 @@ function addCardThree() {
       document.getElementById("cards-four").setAttribute('src', cardImages[(randomCard4-1)])
       total = randomCard1 + randomCard2 + randomCard3 + randomCard4
       displayTotal.textContent = total
-      checkPoints();
+      displayPoints.textContent = checkPoints();
       btnFourthCard.setAttribute('id', "addFourthCard")
       btnFifthCard.removeAttribute('id')
 
@@ -52,28 +49,33 @@ function addCardThree() {
       document.getElementById("cards-five").setAttribute('src', cardImages[(randomCard5-1)])
       total = randomCard1 + randomCard2 + randomCard3 + randomCard4 + randomCard5
       displayTotal.textContent = total
+      displayPoints.textContent = checkPoints();
+      btnFifthCard.setAttribute('id', 'addFifthCard')
+      document.querySelector('.new-game-btn').removeAttribute('id');
   }
 
   function checkPoints() {
     if (total === 19) {
         points += 10
-        nextRoundBtn.removeAttribute("id")
-    } 
-    else if (total ===20) {
+    }
+    else if (total === 20) {
         points += 20
-        nextRoundBtn.removeAttribute("id")
     }
     else if (total === 21) {
         points += 100
-        nextRoundBtn.removeAttribute("id")
-        addCardButton.setAttribute("id", "game-starts")
     }
     else if (total > 21) {
         points -= 100
-        addCardButton.setAttribute("id", "game-starts")
     }
     return points
     }
+   function runGame() {
+       if (points <= 0) {
+          document.querySelector('.class').removeAttribute('id');
+          document.querySelector() 
+       }
+   }
+    
 
 
 
